c5b9883b6a7e6fbda058b8b66645c0513f1269ba
shunyavkash-vivek-sojitra <vivek.sojitra@shunyavkash.com>
2025-03-13 11:58:20 +0530

    Chunk Limit Increased

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,4 +4,19 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/Monaco_Live_Editor",
+
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            if (id.includes("react")) return "react-vendor";
+            if (id.includes("monaco-editor")) return "monaco-editor";
+            return "vendor";
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000,
+  },
 });
